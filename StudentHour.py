00eba72efa899c5bb0a165c57597362682d7b10c
Abhinav C M <176384247+Tyler-Durden06@users.noreply.github.com>
2026-09-17 11:50:58 +0530

    Fix typo in Attendance column name

--- StudentHour.py
+++ StudentHour.py
@@ -46,7 +46,7 @@ if st.button("Predict"):
     # Create input data
     input_data = pd.DataFrame({
         "Study Hours": [study_hours],
-        "Attendance": [Attendance]
+        "Attendence": [attendance]
     })
 
     # Make prediction
